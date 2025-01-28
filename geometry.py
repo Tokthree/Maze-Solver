8327184c2c1c57ff1f41db2d18607d60e23c68b4
Tokthree <TokthreeKF@gmail.com>
2025-01-28 20:14:46 +0000

    Implemented Cell class

--- geometry.py
+++ geometry.py
@@ -3,6 +3,9 @@ class Point:
         self.x = x
         self.y = y
 
+    def __repr__(self):
+        return f"Point Object with co-ordinates x: {self.x}, y: {self.y}"
+
 class Line:
     def __init__(self, pointA, pointB):
         self.pointA = pointA
@@ -10,3 +13,29 @@ class Line:
 
     def draw(self, canvas, fill_colour):
         canvas.create_line(self.pointA.x, self.pointA.y, self.pointB.x, self.pointB.y, fill=fill_colour, width=2)
+
+    def __repr__(self):
+        return f"Line Object from {self.pointA} to {self.pointB}"
+
+class Cell:
+    def __init__(self, has_left_wall = True, has_top_wall = True, has_right_wall = True, has_bottom_wall = True, x1 = 0, y1 = 0, x2 = 0, y2 = 0, win = None):
+        self.has_left_wall = has_left_wall
+        self.has_top_wall = has_top_wall
+        self.has_right_wall = has_right_wall
+        self.has_bottom_wall = has_bottom_wall
+        self.__x1 = x1
+        self.__x2 = x2
+        self.__y1 = y1
+        self.__y2 = y2
+        self.__win = win
+
+    def draw(self):
+        win = self.__win
+        if self.has_left_wall:
+            win.draw_line(Line(Point(self.__x1, self.__y1), Point(self.__x1, self.__y2)), "red")
+        if self.has_top_wall:
+            win.draw_line(Line(Point(self.__x1, self.__y1), Point(self.__x2, self.__y1)), "green")
+        if self.has_right_wall:
+            win.draw_line(Line(Point(self.__x2, self.__y1), Point(self.__x2, self.__y2)), "blue")
+        if self.has_bottom_wall:
+            win.draw_line(Line(Point(self.__x1, self.__y2), Point(self.__x2, self.__y2)), "black")
--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 from window import Window
-from geometry import Point, Line
+from geometry import Point, Line, Cell
 
 def main():
     win = Window(800, 600)
-    win.draw_line(Line(Point(0, 50), Point(750, 50)), "red")
-    win.draw_line(Line(Point(50, 0), Point(50, 550)), "blue")
+    cell1 = Cell(True, True, True, True, 50, 50, 100, 100, win)
+    cell1.draw()
+    cell2 = Cell(True, False, True, False, 110, 50, 160, 100, win)
+    cell2.draw()
+    cell3 = Cell(False, True, False, True, 170, 50, 220, 100, win)
+    cell3.draw()
     win.wait_for_close()
 
 
